70df6880e609303e9e593b1562af3975e0b72fa0
michael-mbugua <kamirimichael369@gmail.com>
2023-05-31 10:54:38 +0300

    minor changes

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-
+import "./homepage.css";
 function Homepage() {
   const logOut = () => {
     localStorage.clear();
     window.location.reload();
   };
   return (
-    <div>
-      Homepage
-      <button onClick={logOut}>Log Out</button>
+    <div className="home__container">
+      <p>
+        Welcome <span className="local"> {localStorage.email}</span>
+      </p>
+      <br />
+      <img src={localStorage.photo} alt="" />
+      <button className="google" onClick={logOut}>
+        Log Out
+      </button>
     </div>
   );
 }
